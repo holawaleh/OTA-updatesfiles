88cb3110aeb09b3e7c2da6bce84cc7a06685d034
holawaleh <holawaleh@gmail.com>
2025-12-01 21:10:05 +0100

    frontend hosted

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
 
 export function authHeaders() {
   return {
